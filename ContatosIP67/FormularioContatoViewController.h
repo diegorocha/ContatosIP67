8f539359aba4757e2101d2eb77872f4357adaa23
ios5065 <ios5065@Saulo.local>
2015-04-11 15:46:57 -0300

    Initial commit

---
+++ ContatosIP67/FormularioContatoViewController.h
@@ -0,0 +1,23 @@
+//
+//  ViewController.h
+//  ContatosIP67
+//
+//  Created by ios5065 on 11/04/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FormularioContatoViewController : UIViewController
+
+@property IBOutlet UITextField *nome;
+@property IBOutlet UITextField *telefone;
+@property IBOutlet UITextField *email;
+@property IBOutlet UITextField *endereco;
+@property IBOutlet UITextField *site;
+
+
+-(IBAction)pegaDadosDoFormulario;
+
+@end
+
---
+++ ContatosIP67/FormularioContatoViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  ContatosIP67
+//
+//  Created by ios5065 on 11/04/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import "FormularioContatoViewController.h"
+#import "Contato.h"
+
+@interface FormularioContatoViewController ()
+
+@end
+
+@implementation FormularioContatoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pegaDadosDoFormulario{
+    NSString *nome = [self.nome text];
+    NSString *telefone = [self.telefone text];
+    NSString *email = [self.email text];
+    NSString *endereco = [self.endereco text];
+    NSString *site = [self.site text];
+    
+    Contato *contato = [Contato new];
+    contato.nome = nome;
+    contato.telefone = telefone;
+    contato.email = email;
+    contato.endereco = endereco;
+    contato.site = site;
+    
+    NSLog(@"%@", contato);
+}
+
+@end
