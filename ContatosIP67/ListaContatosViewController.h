5ef59cbaec2a2e29f1610c0ca5ec94ebe9eb10e0
ios5065 <ios5065@Saulo.local>
2015-04-18 11:33:54 -0300

    Adicionado UITableViewController

---
+++ ContatosIP67/ListaContatosViewController.h
@@ -0,0 +1,14 @@
+//
+//  ListaContatosViewController.h
+//  ContatosIP67
+//
+//  Created by ios5065 on 18/04/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ListaContatosViewController : UITableViewController
+
+@end
---
+++ ContatosIP67/ListaContatosViewController.m
@@ -0,0 +1,13 @@
+//
+//  ListaContatosViewController.m
+//  ContatosIP67
+//
+//  Created by ios5065 on 18/04/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import "ListaContatosViewController.h"
+
+@implementation ListaContatosViewController
+
+@end
