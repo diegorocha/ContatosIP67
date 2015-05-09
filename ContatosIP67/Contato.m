9fea5fe4d5c7d0eac8d58769e3f275026d3cd7eb
Diego Rocha <diego@diegorocha.com.br>
2015-05-09 14:13:58 -0300

    Exercício 17.9 - Contatos como NSManagedObject

--- ContatosIP67/Contato.m
+++ ContatosIP67/Contato.m
@@ -10,6 +10,8 @@
 
 @implementation Contato
 
+@dynamic nome, telefone, email, endereco, site, lat, lng, foto;
+
 -(NSString *)description{
     return [NSString stringWithFormat:@"Nome: %@, Telefone: %@, E-mail: %@, Endereço: %@, Site: %@;", self.nome, self.telefone, self.email, self.endereco, self.site];
 }
